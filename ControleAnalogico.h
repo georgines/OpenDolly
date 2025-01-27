f0ab5c168ecc4b40af703bee721b05bb7e1d8641
Georgines Bezerra Pereira <geoginae.p2@gmail.com>
2025-01-26 23:05:12 -0300

    correcao do joystick

--- ControleAnalogico.h
+++ ControleAnalogico.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdio.h>
+#include <math.h>
+#include <vector>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 
@@ -10,12 +12,16 @@ class ControleAnalogico {
     uint8_t canal;
     float entradaMin, entradaMax, saidaMin, saidaMax;
     float ultimoValor;
+    std::vector<float> bufferLeituras;
+    size_t tamanhoFiltro;
+    float limiar; // Limiar de tolerância para mudanças
 
     float mapearFloat(float valor, float entrada_min, float entrada_max, float saida_min, float saida_max);
-    float lerValorAtual();
 
   public:
-    ControleAnalogico(uint8_t pino_adc, uint8_t canal_adc, float saida_min, float saida_max);
+    float lerValorAtual();
+    ControleAnalogico(uint8_t pino_adc, uint8_t canal_adc, float saida_min, float saida_max, size_t tamanho_filtro = 5, float limiar_tolerancia = 0.5f);
     bool houveAlteracao();
     float lerAlteracao();
+    float aplicarFiltro(float novoValor);
 };
--- auxiliarJoystick.h
+++ auxiliarJoystick.h
@@ -8,7 +8,7 @@ void controlarMotoresPeloJoystick(Sistema &sistema)
 {
     if (sistema.joystick_X.houveAlteracao())
     {
-        float valorX = sistema.joystick_X.lerAlteracao();
+        int32_t valorX = static_cast<int32_t>(sistema.joystick_X.lerValorAtual());
         int32_t passo_min = calcularPassosParaDeslocamento(DESLOCAMENTO_MIN_MOTOR_X);
         int32_t passo_maximo = calcularPassosParaDeslocamento(DESLOCAMENTO_MAXIMO_MOTOR_X);
         moverMotorLivre(&sistema.motorX, &valorX, passo_min, passo_maximo, 'X');
@@ -16,7 +16,7 @@ void controlarMotoresPeloJoystick(Sistema &sistema)
 
     if (sistema.joystick_Y.houveAlteracao())
     {
-        float valorY = sistema.joystick_Y.lerAlteracao();
+        int32_t valorY = static_cast<int32_t>(sistema.joystick_Y.lerValorAtual());
         int32_t angulo_min = calcularPassosParaInclinacao(ANGULACAO_MIN_MOTOR_Y);
         int32_t angulo_max = calcularPassosParaInclinacao(ANGULACAO_MAXIMO_MOTOR_Y);
         moverMotorLivre(&sistema.motorY, &valorY, angulo_min, angulo_max, 'Y');
